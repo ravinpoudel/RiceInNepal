c2358da19fddeae263358eac6e485042964373c6
RavinP <ravinp@xbiome.com>
2024-05-28 15:53:21 +0545

    updates

--- Untitled.R
+++ Untitled.R
@@ -29,3 +29,58 @@ gg_all <- ggplot(df, aes(x = "", y = Value, color = Treatment)) +
 gg_all
 ggsave("within_total_sum_of_squares.pdf", gg_all, height = 4, width = 4)
 
+
+
+
+
+
+
+print(vector)
+# Load required library
+library(stringdist)
+
+# Define the vector
+vector <- c("NARC",
+            "MoALD",
+            "PU",
+            "Private Co",
+            "iDE",
+            "MBU",
+            "Agrovet",
+            "SQCC",
+            "NPDA",
+            "TU",
+            "NRRP",
+            "AFU", 
+            "China",
+            "India",
+            "Bangladesh")
+# Define the custom function to calculate pairwise string distances
+pairwise_distance <- function(x, y) {
+  stringdist::stringdist(x, y, method = "jaccard")
+}
+
+# Create an empty matrix
+n <- length(vector)
+pairwise_matrix <- matrix(0, nrow = n, ncol = n)
+
+# Fill the lower triangle of the matrix with pairwise distances
+for (i in 1:(n - 1)) {
+  for (j in (i + 1):n) {
+    pairwise_matrix[j, i] <- " "
+  }
+}
+
+# Fill the upper half of the matrix with "N/A"
+pairwise_matrix[upper.tri(pairwise_matrix)] <- "N/A"
+
+# Fill the diagonal with NA
+diag(pairwise_matrix) <-"N/A"
+
+# Print the half lower triangle matrix with upper half as "N/A"
+print(pairwise_matrix)
+rownames(pairwise_matrix) <- vector
+colnames(pairwise_matrix) <- vector
+pairwise_matrix
+
+write.csv(pairwise_matrix, "pairwise_matrix.csv")
